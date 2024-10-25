b0074d428f9b71678b086c5827b15a0279876ad4
fiza <fiiza11000@gmail.com>
2024-10-26 00:13:54 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProductComponent } from './pages/product/product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path:'',component: HomeComponent
+  },
+  {
+    path:'login',component:LoginComponent
+  },
+  {
+    path:'products/:id',component: ProductComponent
+  },
+
+];
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../../api.service';
+import { RouterLink } from '@angular/router';
+import { SlicePipe } from '@angular/common';
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [SlicePipe,RouterLink],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+constructor(private api: ApiService) { }
+products: any= [ ]
+
+
+ngOnInit() {
+  this.api.getData().subscribe((data: any) =>{
+    this.products = data;
+})
+}
+
+}
